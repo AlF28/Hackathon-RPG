0722275b08e8bb5352c8cdfd574df7d8f881ee71
harazara <123052299+harazara@users.noreply.github.com>
2023-01-29 02:30:16 -0500

    Add files via upload

---
+++ boss2 dialogue.py
@@ -0,0 +1,17 @@
+player = input("Enter player name:")
+
+print(f"{player} slowly opens the door to the next room, checking for another anthropomorphic animal.")
+print(f"{player} determines the coast is clear and tip toes in.")
+print("Thud!")
+print(f"Suddenly {player} is lying on the floor when a figure appears out of the shaows.")
+print(f"{player} strains their head to look up at the tall, lanky alien standing in front of them. ")
+print("\"Hello, human. You can call me Professor Z.\" the alien said plainly.")
+print("\"I know you're thinking 'I don't care who you are. Let me go!', but let me finish.\" said Professor Z")
+print("Professor Z turned around and pulled out what seemed to be chalk board that uses lasers.")
+print("\"What you're wrapped in is my infalliable zaralegg fiber silk. I'm the first one to synthesize g chain amin... \"")
+print(f"{player} could feel the silk melting, allowing them to slowly pull it off during the lecture.")
+print("\"So that's all to say that my invention is perfect for capturing human speci-\"")
+print("Professor Z turned around to find his specimen had freed themselves from the special unbreakable fiber.")
+print("\"Impossible! I've been working on this for eleventeen gorbles. Well, now I have to kill you.\" Professor Z announced.")
+
+#cue fight scene
